c6e1374d34cf00203bbc112892789f8b7dd42d92
Kieron Papps <darkeninglight@hotmail.co.uk>
2018-08-29 16:45:42 +0100

    Resolving Issues
    *Edited CardString
    	- Resolve issue #19
    	- Resolve issue #18
    	- Resolve issue #17
    
    *Edited PaymentAccepted
    	- Resolve issue #16
    
    *Edited Rainmaker.
    	- Resolve issue #9

--- src/main/java/gsmith/cards/Rainmaker.java
+++ src/main/java/gsmith/cards/Rainmaker.java
@@ -20,7 +20,7 @@ public class Rainmaker extends CustomCard {
 	private static final CardStrings cardStrings = CardCrawlGame.languagePack.getCardStrings(ID);
 	public static final String NAME = cardStrings.NAME;
 	public static final String DESCRIPTION = cardStrings.DESCRIPTION;
-	private static final int COST = 1;
+	private static final int COST = 2;
 	
 	private static final int ENERGY_GAIN = 1;
 	private static final int UPGRADE_INCREASE_ENERGY = 1;
