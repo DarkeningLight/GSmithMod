88ac75be65d402ed216ac07eca93ed01807cb105
Kieron Papps <darkeninglight@hotmail.co.uk>
2018-09-11 17:46:15 +0100

    Resolves issue #2
    *Edited StrikeD
    	- Gave Strike ID with BaseMod
    
    Now works with Vampire & Back to basics.

--- src/main/java/gsmith/cards/StrikeD.java
+++ src/main/java/gsmith/cards/StrikeD.java
@@ -11,6 +11,8 @@ import com.megacrit.cardcrawl.localization.CardStrings;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
 
 import basemod.abstracts.CustomCard;
+import basemod.helpers.BaseModTags;
+import basemod.helpers.CardTags;
 import gsmith.GSmithMod;
 import gsmith.patches.AbstractCardEnum;
 
@@ -32,6 +34,7 @@ public class StrikeD extends CustomCard {
 				AbstractCardEnum.GOLD, AbstractCard.CardRarity.BASIC, AbstractCard.CardTarget.ENEMY);
 		
 		this.baseDamage = ATTACK_DMG;
+		CardTags.addTags(this, BaseModTags.BASIC_STRIKE);
 	}
 
 	@Override
