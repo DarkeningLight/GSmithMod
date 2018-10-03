8db815a59d49b44d60bd469070932d92416484d1
Kieron Papps <darkeninglight@hotmail.co.uk>
2018-10-03 18:58:13 +0100

    Changed Five Finger Discount
    *Edited FiveFingerDiscount.
    
    Old: Gave gold when monster is killed.
    
    NEW: Gives discounts in discard pile.

--- src/main/java/gsmith/cards/FiveFingerDiscount.java
+++ src/main/java/gsmith/cards/FiveFingerDiscount.java
@@ -2,6 +2,7 @@ package gsmith.cards;
 
 import com.megacrit.cardcrawl.actions.AbstractGameAction;
 import com.megacrit.cardcrawl.actions.common.DamageAction;
+import com.megacrit.cardcrawl.actions.common.MakeTempCardInDiscardAction;
 import com.megacrit.cardcrawl.cards.AbstractCard;
 import com.megacrit.cardcrawl.cards.DamageInfo;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
@@ -12,7 +13,6 @@ import com.megacrit.cardcrawl.monsters.AbstractMonster;
 
 import basemod.abstracts.CustomCard;
 import gsmith.GSmithMod;
-import gsmith.actions.GainGoldAction;
 import gsmith.patches.AbstractCardEnum;
 
 public class FiveFingerDiscount extends CustomCard {
@@ -21,12 +21,11 @@ public class FiveFingerDiscount extends CustomCard {
 	private static final CardStrings cardStrings = CardCrawlGame.languagePack.getCardStrings(ID);
 	public static final String NAME = cardStrings.NAME;
 	public static final String DESCRIPTION = cardStrings.DESCRIPTION;
+	public static final String UPGRADE_DESCRIPTION = cardStrings.UPGRADE_DESCRIPTION;
 	private static final int COST = 1;
 	
-	private static final int ATTACK_DMG = 1;
-	private static final int NUM_TIME = 5;
-	private static final int GOLD_GAIN = 10;
-	private static final int UPGRADE_PLUS_GOLD = 10;
+	private static final int ATTACK_DMG = 5;
+	private static final int DISCOUNT_NUM = 2;
 	
 	public static final String PATH = "cards/five_finger_discount.png";
 	
@@ -35,7 +34,7 @@ public class FiveFingerDiscount extends CustomCard {
 				AbstractCardEnum.GOLD, AbstractCard.CardRarity.UNCOMMON, AbstractCard.CardTarget.ENEMY);
 		
 		this.baseDamage = this.damage = ATTACK_DMG;
-		this.baseMagicNumber = this.magicNumber = GOLD_GAIN;
+		this.exhaust = true;
 	}
 
 	@Override
@@ -47,24 +46,25 @@ public class FiveFingerDiscount extends CustomCard {
 	public void upgrade() {
 		if (!this.upgraded) {
 			this.upgradeName();
-			this.upgradeMagicNumber(UPGRADE_PLUS_GOLD);
+			
+			this.rawDescription = UPGRADE_DESCRIPTION;
+			initializeDescription();
 		}
 
 	}
 
 	@Override
 	public void use(AbstractPlayer player, AbstractMonster monster) {
-		
-		for (int i = 1 ; i <= NUM_TIME ; i++) {
+		AbstractDungeon.actionManager.addToBottom(new DamageAction(monster, 
+				new DamageInfo(player, this.damage, this.damageTypeForTurn), 
+				AbstractGameAction.AttackEffect.BLUNT_LIGHT));
+		if (this.upgraded) {
 			AbstractDungeon.actionManager.addToBottom(new DamageAction(monster, 
 					new DamageInfo(player, this.damage, this.damageTypeForTurn), 
 					AbstractGameAction.AttackEffect.BLUNT_LIGHT));
 		}
 		
-		if (monster.isDead) {
-			AbstractDungeon.actionManager.addToBottom(new GainGoldAction(player, this.magicNumber));
-		}
-			
+		AbstractDungeon.actionManager.addToBottom(new MakeTempCardInDiscardAction(new Discount(), DISCOUNT_NUM));
 	}
 
 }
